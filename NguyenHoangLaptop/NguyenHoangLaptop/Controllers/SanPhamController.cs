ef61e78dad9f4519d47b4d6c78ba02c5d7b7d3a5
Nguyễn Danh Thái <47393239+tdpndthai@users.noreply.github.com>
2019-04-15 22:00:20 +0700

    Demo web chua co chuc nang gi ca

---
+++ NguyenHoangLaptop/NguyenHoangLaptop/Controllers/SanPhamController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NguyenHoangLaptop.Models;
+
+namespace NguyenHoangLaptop.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        QuanlibanhanglaptopEntities1 db = new QuanlibanhanglaptopEntities1();
+        // GET: SanPham
+        public ActionResult SanPham()
+        {
+            //lấy dữ liệu nạp vào model
+            var lstLaptopDell = db.SanPham.Where(n => n.MaLoaiSP == 1 && n.Moi == 1);
+            ViewBag.ListDell = lstLaptopDell;
+            var lstLaptopHP = db.SanPham.Where(n => n.MaLoaiSP == 2 && n.Moi == 1);
+            ViewBag.ListHP = lstLaptopHP;
+            return View();
+        }
+        public ActionResult SanPhamPartial()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ NguyenHoangLaptop/NguyenHoangLaptop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NguyenHoangLaptop.MvcApplication" Language="C#" %>
