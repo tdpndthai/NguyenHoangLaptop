107dd140b48d2298225e421770b9fc183147cf29
Nguyễn Danh Thái <47393239+tdpndthai@users.noreply.github.com>
2019-04-23 11:23:25 +0700

    Demo
    demo trang chi tiết sản phẩm,danh sách sản phẩm

--- NguyenHoangLaptop/NguyenHoangLaptop/Controllers/SanPhamController.cs
+++ NguyenHoangLaptop/NguyenHoangLaptop/Controllers/SanPhamController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using NguyenHoangLaptop.Models;
@@ -11,18 +12,41 @@ namespace NguyenHoangLaptop.Controllers
     {
         QuanlibanhanglaptopEntities1 db = new QuanlibanhanglaptopEntities1();
         // GET: SanPham
-        public ActionResult SanPham()
-        {
-            //lấy dữ liệu nạp vào model
-            var lstLaptopDell = db.SanPham.Where(n => n.MaLoaiSP == 1 && n.Moi == 1);
-            ViewBag.ListDell = lstLaptopDell;
-            var lstLaptopHP = db.SanPham.Where(n => n.MaLoaiSP == 2 && n.Moi == 1);
-            ViewBag.ListHP = lstLaptopHP;
-            return View();
-        }
         public ActionResult SanPhamPartial()
         {
             return PartialView();
+        }
+        public ActionResult ChiTietSanPham(int? id)
+        {
+            //kiểm tra xem tham số truyền vào có rỗng hay không?
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //nếu ko thì truy xuất csdl lấy id sản phẩm
+            SanPham sp = db.SanPham.SingleOrDefault(n => n.MaSP == id && n.DaXoa==true);
+            if (sp == null)
+            {
+                //thông báo nếu ko có sp đó
+                return HttpNotFound();
+            }
+            return View(sp);
+        }
+        //load sp theo mã loại sp và mã nsx
+        public ActionResult SanPham(int? MaLoaiSP,int? MaNSX)
+        {
+            if (MaLoaiSP == null || MaNSX == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            // load sp theo tiêu chí mã loại sp và mã nsx
+            var lstSanPham = db.SanPham.Where(n => n.MaLoaiSP == MaLoaiSP && n.MaNSX == MaNSX);
+            if (lstSanPham.Count() == 0)
+            {
+                //thông báo nếu ko có sp
+                return HttpNotFound();
+            }
+            return View(lstSanPham);
         }
     }
 }
